b5f0bef4db312bce44f43a5dd335d5937bc130c3
DarkProtocol <svechnick@mail.ru>
2017-03-31 20:54:00 +0300

    Valera pes @@@

--- complex_kushtuev.c
+++ complex_kushtuev.c
@@ -5,29 +5,29 @@
 typedef struct {
 	float Re;
 	float Im;
-} complexNumber;
+} complexNumber_t;
 
-complexNumber * complexSum (complexNumber * firstNumber, complexNumber * secondNumber) {
+complexNumber_t * complexSum (complexNumber_t * firstNumber, complexNumber_t * secondNumber) {
 
-	complexNumber * result = (complexNumber*) malloc(sizeof(complexNumber));
+	complexNumber_t * result = (complexNumber_t*) malloc(sizeof(complexNumber_t));
 	result->Re = firstNumber->Re + secondNumber->Re;
 	result->Im = firstNumber->Im + secondNumber->Im;
 
 	return result;
 }
 
-complexNumber * complexSub (complexNumber * firstNumber, complexNumber * secondNumber) {
+complexNumber_t * complexSub (complexNumber_t * firstNumber, complexNumber_t * secondNumber) {
 
-	complexNumber * result = (complexNumber*) malloc(sizeof(complexNumber));
+	complexNumber_t * result = (complexNumber_t*) malloc(sizeof(complexNumber_t));
 	result->Re = firstNumber->Re - secondNumber->Re;
 	result->Im = firstNumber->Im - secondNumber->Im;
 
 	return result;
 }
 
-complexNumber * complexDiv (complexNumber * firstNumber, complexNumber * secondNumber) {
+complexNumber_t * complexDiv (complexNumber_t * firstNumber, complexNumber_t * secondNumber) {
 
-	complexNumber * result = (complexNumber*) malloc(sizeof(complexNumber));
+	complexNumber_t * result = (complexNumber_t*) malloc(sizeof(complexNumber_t));
 
 	result->Re = firstNumber->Re * secondNumber->Re + firstNumber->Im * secondNumber->Im; 
 	result->Re /= pow(secondNumber->Re, 2) + pow(secondNumber->Im, 2);
@@ -38,9 +38,9 @@ complexNumber * complexDiv (complexNumber * firstNumber, complexNumber * secondN
 	return result;
 }
 
-complexNumber * complexMult (complexNumber * firstNumber, complexNumber * secondNumber) {
+complexNumber_t * complexMult (complexNumber_t * firstNumber, complexNumber_t * secondNumber) {
 
-	complexNumber * result = (complexNumber*) malloc(sizeof(complexNumber));
+	complexNumber_t * result = (complexNumber_t*) malloc(sizeof(complexNumber_t));
 
 	result->Re = firstNumber->Re * secondNumber->Re - firstNumber->Im * secondNumber->Im; 
 	result->Im = firstNumber->Im * secondNumber->Re + firstNumber->Re * secondNumber->Im; 
@@ -51,13 +51,13 @@ complexNumber * complexMult (complexNumber * firstNumber, complexNumber * second
 
 int main(void) {
 
-	complexNumber * firstNumber = (complexNumber*) malloc(sizeof(complexNumber));
-	complexNumber * secondNumber = (complexNumber*) malloc(sizeof(complexNumber));
+	complexNumber_t * firstNumber = (complexNumber_t*) malloc(sizeof(complexNumber_t));
+	complexNumber_t * secondNumber = (complexNumber_t*) malloc(sizeof(complexNumber_t));
 
-	complexNumber * sum = (complexNumber*) malloc(sizeof(complexNumber));
-	complexNumber * sub = (complexNumber*) malloc(sizeof(complexNumber));
-	complexNumber * div = (complexNumber*) malloc(sizeof(complexNumber));
-	complexNumber * mult = (complexNumber*) malloc(sizeof(complexNumber));
+	complexNumber_t * sum = (complexNumber_t*) malloc(sizeof(complexNumber_t));
+	complexNumber_t * sub = (complexNumber_t*) malloc(sizeof(complexNumber_t));
+	complexNumber_t * div = (complexNumber_t*) malloc(sizeof(complexNumber_t));
+	complexNumber_t * mult = (complexNumber_t*) malloc(sizeof(complexNumber_t));
 
 	firstNumber->Re = 4;
 	firstNumber->Im = 3;
